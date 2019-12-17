7eb0e8f7bb2acefe3a3a7f037dcd7098709d261b
blahu77 <blahu77@gmail.com>
2019-12-17 12:34:28 -0800

    its class 6 stupid!

---
+++ SOLUTIONS/CLASS6/2/a.py
@@ -0,0 +1,36 @@
+"""
+12345678901234567890123456789012345678901234567890123456789012345678901234567890
+"""
+from nornir import InitNornir
+from nornir.core.filter import F
+from nornir.core.task import Result
+from nornir.plugins.tasks import networking, text
+from nornir.plugins.functions.text import print_result
+
+from pprint import pprint as pp
+
+def get_command(task):
+    r = task.run(
+        task=text.template_file,
+        template="loopback.j2",
+        path="./",
+        **task.host
+    )
+    #import ipdb; ipdb.set_trace()
+    if 'syntax error' in r.result:
+        print("?")
+        raise ValueError("Junos says syntax error")
+
+
+def main():
+    nr = InitNornir(config_file="./config_a.yaml")
+    nr = nr.filter(F(groups__contains="nxos"))
+    pp(nr.inventory.hosts.items())
+
+    results = nr.run(task=get_command)
+    print_result(results)
+
+    print("Success!")
+
+if __name__ == '__main__':
+    main()
